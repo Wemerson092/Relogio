7c73b78a9e04c436f86aca520e6f9348b022d974
Wemerson <wemersonwps092@gmail.com>
2022-08-12 13:13:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time() {
+    let time = new Date()
+    let hr =time.getHours()
+    let min = time.getMinutes()
+    let seg = time.getSeconds()
+
+    if(hr < 10) hr = '0' + hr;
+
+    if(min < 10) min = '0' + min;
+
+    if(seg < 10) seg = '0' + seg;
+
+    horas.innerHTML = hr
+    minutos.innerHTML = min
+    segundos.innerHTML = seg
+    
+})
+
